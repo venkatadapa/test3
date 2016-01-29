486d93df542c50e4306795b08d9abb46c0bcf02e
venkatadapa <venki.adapa@gmail.com>
2016-01-29 11:30:07 +0530

    Added code to get vote audit info

---
+++ civiq_ballots/templates/cb-votes.tpl.php
@@ -0,0 +1,64 @@
+<div class="row">
+  <div class="col-md-12"><?php echo $question['question']; ?></div>
+</div>
+
+<div class="row">
+  <div class="col-md-12"><?php echo $question['description']; ?></div>
+</div>
+
+<!--How many users cast votes and what is the preference for each option-->
+<h2><?php echo t('vote audit'); ?></h2>
+<table>
+<thead>
+  <tr>	
+    <th rowspan="2"><?php echo t('Preferences'); ?></th>
+    <th colspan="<?php echo count($options); ?>"><?php echo t('Options'); ?></th>
+    <th rowspan="2"><?php echo t('Points Excercised'); ?></th>
+    <th rowspan="2"><?php echo t('Points Not Used'); ?></th>
+  </tr>
+  <tr>
+    <?php foreach($options as $option) { ?>
+		<th> <?php echo $option; ?> </th>
+    <?php } ?>
+  </tr>
+</thead>	
+<?php $vote_key = 1; 
+  foreach($votes as $vote) { dsm($vote); ?>
+	<tr>
+	  <td><?php echo $vote_key; ?></td>
+	  <?php for ($key=1; $key <= count($options); $key++) { ?>
+		<td> <?php echo $vote['votes'][$key]; ?> </td>
+      <?php } ?>
+	  <td><?php echo $vote['total_points']; ?></td>
+	  <td><?php echo $vote['points_unused']; ?></td>
+	</tr>
+<?php $vote_key++;
+  } ?>
+</table>
+
+
+<!--Each preference given how many times to each option-->
+<h2><?php echo t('voters profile'); ?></h2>
+<table>
+<thead>
+  <tr>	
+    <th rowspan="2"><?php echo t('Preferences'); ?></th>
+    <th colspan="<?php echo count($options); ?>"><?php echo t('Options'); ?></th>
+    <th rowspan="2"><?php echo t('Totals'); ?></th>    
+  </tr>
+  <tr>
+    <?php foreach($options as $option) { ?>
+		<th> <?php echo $option; ?> </th>
+    <?php } ?>
+  </tr>
+</thead>	
+<?php 
+  foreach($options as $option) { 
+	
+  }
+?>
+</table>
+
+
+<!--Ranking based on preferences choosen-->
+<h2><?php echo t('social choice and social ranking'); ?></h2>
